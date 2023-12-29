685356dfd02adc5be7a02ea80b7f69f233e504f6
Marcos Renê <renemoura789@gmail.com>
2023-12-29 12:11:56 -0300

    refactor: add new theme root

--- clients/src/App.tsx
+++ clients/src/App.tsx
@@ -1,16 +1,16 @@
 import { FileVideo, Github, Upload, Wand2 } from 'lucide-react';
-import { Button } from './components/ui/button';
-import { Separator } from './components/ui/separator';
-import { Textarea } from './components/ui/textarea';
-import { Label } from './components/ui/label';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { Textarea } from '@/components/ui/textarea';
+import { Label } from '@/components/ui/label';
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from './components/ui/select';
-import { Slider } from './components/ui/slider';
+} from '@/components/ui/select';
+import { Slider } from '@/components/ui/slider';
 
 export function App() {
   return (
